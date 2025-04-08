7ef5f773df032d3868d7cf7f478dda61dc9ecf53
Pla-ThanosXD <estebanmontoya983@gmail.com>
2025-04-08 12:54:47 -0500

    subir

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const images = document.querySelectorAll('.imagenes img');
+
+images.forEach(image => {
+  image.addEventListener('click', function() {
+    const card = image.parentElement;
+    const targetId = image.getAttribute('data-target');
+    let targetContent = document.getElementById(targetId);
+    
+    if (!targetContent) {
+      targetContent = card.querySelector('.' + targetId);
+    }
+    
+    if (targetContent) {
+      if (targetContent.classList.contains('open')) {
+        targetContent.classList.remove('open');
+        card.classList.remove('open');
+      } else {
+        targetContent.classList.add('open');
+        card.classList.add('open');
+      }
+    }
+  });
+});
